38199270ca6149faf9fde596386fbd6ca28aad56
Your GitHub Name <lujain.aljehani@outlook.com>
2025-11-05 11:47:47 +0300

    TODO4

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,20 @@
 export function add(numbers) {
-  return {/*write code here*/}
+    return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+    return numbers.reduce((result, n) => result - n);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+    return numbers.reduce((result, n) => result * n, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+    if (numbers.length === 1) return numbers[0];
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        result /= numbers[i];
+    }
+    return result;
 }
-
